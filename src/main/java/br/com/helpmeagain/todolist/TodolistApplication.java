2d15f4cc39ec0a9db594327cc20fe6a0514d410c
Felipe Marques <felipe.comarques1208@gmail.com>
2023-10-12 16:53:52 -0300

    feat: Database integration

--- src/main/java/br/com/helpmeagain/todolist/TodolistApplication.java
+++ src/main/java/br/com/helpmeagain/todolist/TodolistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodolistApplication {
-
+	// http://localhost:8080/h2-console
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
